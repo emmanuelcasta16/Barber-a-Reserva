db3a35f2239eeaca655a47fc831a343b1bf6bdae
emmanuelcasta16 <emmanuel.c1913@gmail.com>
2026-03-30 13:12:26 -0500

    feat: add vite.config.js - Barber-a-Reserva app

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        product: resolve(__dirname, 'product.html'),
+        checkout: resolve(__dirname, 'checkout.html'),
+      },
+    },
+  },
+})
